240d3dcabd1029350347fd1b12c8b13046268caf
TheCPP <80353816+toni5757@users.noreply.github.com>
2022-11-20 14:19:37 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #assemble boot.s file
-as --32 src/boot.asm -o bin/boot.o
+as --32 boot.asm -o boot.o
 
 #compile kernel.c file
-gcc -m32 -c bin/kernel.c -o bin/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 #linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T linker.ld bin/kernel.o bin/boot.o -o bin/MyOS.bin -nostdlib
+ld -m elf_i386 -T linker.ld kernel.o boot.o -o MyOS.bin -nostdlib
 
 #check MyOS.bin file is x86 multiboot file or not
-grub-file --is-x86-multiboot bin/MyOS.bin
+grub-file --is-x86-multiboot MyOS.bin
 
 #building the iso file
-mkdir -p bin/isodir/boot/grub
-cp bin/MyOS.bin bin/isodir/boot/MyOS.bin
-cp bin/grub.cfg bin/isodir/boot/grub/grub.cfg
-grub-mkrescue -o bin/MyOS.iso bin/isodir
+mkdir -p isodir/boot/grub
+cp MyOS.bin isodir/boot/MyOS.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o MyOS.iso isodir
 
 #run it in qemu
-qemu-system-x86_64 -cdrom bin/MyOS.iso
+qemu-system-x86_64 -cdrom MyOS.iso
